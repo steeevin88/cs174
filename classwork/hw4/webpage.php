e272be11faf674f94e35e7c310ab03facbfdc384
Steven Le <stevenleusa79@yahoo.com>
2023-10-21 19:24:27 -0700

    hw4

--- classwork/hw4/webpage.php
+++ classwork/hw4/webpage.php
@@ -14,89 +14,84 @@
     </html>
     _END;
 
+    // define constant for MySQL errors
+    define("FATAL_ERROR", "Sorry, but there has been an error is completing the requested task. 
+    Please to refresh your browser and try again. Thank you.");
+
     // retrieve the content in login.php, connect to our database
     require_once 'login.php';
     $conn = new mysqli($hn, $un, $pw, $db); 
-    if ($conn->connect_error) die(mysql_fatal_error());
+    if ($conn->connect_error) {
+        die(dieMessage()); // connection failure
+    }
 
+    // check for files table (create it if it doesn't exist...)
+    $query = "SHOW TABLES LIKE 'files'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows == 0) { // doesn't exist, so create the table
+        $query = "CREATE TABLE files(title VARCHAR(255),content TEXT) ENGINE=InnoDB;";
+        $result = $conn->query($query);
+        if (!$result) {
+            die(dieMessage());
+        }
+    }
+    $result->close();
+    
+    // print pre-existing entries in the database
     $query = "SELECT * FROM files";
     $result = $conn->query($query);
-    if (!$result || ($rows = $result->num_rows) == 0) echo "No entries yet.";
-    for ($i = 0; $i < $rows; $i++) {
-        /* print line by line
-            // print the text input of entry i
+    if (!$result) {
+        die(dieMessage()); // failed to connected to table "files"
+    } else {
+        // print entries in database upon successful connection
+        for ($i = 0; $i < $rows = $result->num_rows; $i++) {
+            /* print line by line
+                // print the text input of entry i
+                $result->data_seek($i);
+                echo 'Name: '.$result->fetch_assoc()['name'].'<br>';
+    
+                // print the file content of entry i
+                $result->data_seek($i);
+                echo 'Content: '.$result->fetch_assoc()['content'].'<br>';
+            */
+            // grab a row entry in our database
             $result->data_seek($i);
-            echo 'Name: '.$result->fetch_assoc()['name'].'<br>';
-
-            // print the file content of entry i
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            // print from the 'name' column
+            echo 'Title: '.$row['title'].'<br>';
+            // print from the 'content' column
             $result->data_seek($i);
-            echo 'Content: '.$result->fetch_assoc()['content'].'<br>';
-        */
-
-        // grab a row entry in our database
-        $result->data_seek($i);
-        $row = $result->fetch_array(MYSQLI_ASSOC);
-        // print from the 'name' column
-        echo 'Title '.($i+1).': '.$row['title'].'<br>';
-        // print from the 'content' column
-        $result->data_seek($i);
-        echo 'Content '.($i+1).': '.$row['content'].'<br><br>';
-
+            echo 'Content: '.$row['content'].'<br><br>';
+    
+        }
     }
     $result->close();
     $conn->close();
+    
+    function dieMessage() {
+        echo <<<EOD
+        FATAL_ERROR: Sorry, but there has been an error is completing the requested task.
+        Please refresh your browser and try again. Thank you.
+        EOD;
+    }    
 
-    function mysql_fatal_error() {
-        return 'Sorry, but there has been an error is completing the requested task. 
-        Please to refresh your browser and try again. Thank you.';
+    // my PHP version doesn't support get_magic_quotes_gpc(), so I opted to use these functions instead
+    function sanitizeString($var) {
+        $var = stripslashes($var);
+        $var = strip_tags($var);
+        $var = htmlentities($var);
+        return $var;
     }
 
-    function get_post($conn, $var) {
-		return $conn->real_escape_string($_POST[$var]);
-	}
+    function sanitizeMySQL($connection, $var) {
+        $var = $connection->real_escape_string($var);
+        $var = sanitizeString($var);
+        return $var;
+    }
 
-    if ($_FILES && isset($_POST['title'])) {
-        // reopen connection to database
-        $conn = new mysqli($hn, $un, $pw, $db); 
-
-        // get file input
-        // sanitize super global variable
-        $name = htmlentities($_FILES['filename']['tmp_name']);
-        // validate file name
-        $name = strtolower(preg_replace("[^A-Za-z0-9]", "", $name));
-        // open file
-        $fh = fopen($name, 'r') or die("File does not exist or you lack permission to open it");
-        // sanitize file input
-        $sanitizedInput = htmlentities(fgets($fh));
-        
-        // get title input
-        $title = get_post($conn, 'title');
-
-        // issue query
-        $query = "INSERT INTO files (title, content) VALUES ('$title', '$sanitizedInput')";
-        $result = $conn->query($query);
-
-        if (!$result) echo "INSERT failed: " . $conn->error;
-        else { // display new entry
-            $query = "SELECT * FROM files";
-            $result = $conn->query($query);
-            
-            // grab the last row entry in our database --> this is our new row
-            $last_row = ($result->num_rows) - 1;
-            $result->data_seek($last_row);
-            $row = $result->fetch_array(MYSQLI_ASSOC);
-            // print from the 'name' column
-            echo 'Title '.($i+1).': '.$row['title'].'<br>';
-            // print from the 'content' column
-            $result->data_seek($i);
-            echo 'Content '.($i+1).': '.$row['content'].'<br><br>';
-        }
-        $result->close();
-
-        // close file
-        fclose($fh);
-
-        // close database connection
-        $conn->close();
+    // check if file is uploaded
+    if ($_FILES) {
+        require_once 'helper.php';
     }
 ?>  
